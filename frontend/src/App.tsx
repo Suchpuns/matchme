103c209a9883df61d5b9b77a3115ee2fe8ec4f96
CHUN Nicole Wai Yan <19chunnw3@Nicole-Chun-MacBookPro-2.local>
2024-03-02 11:11:31 +1100

    routing setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router'
+
+import EventCreation from "./pages/admin/EventCreation.tsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   useEffect(() => {
     fetch("http://127.0.0.1:5000")
       .then(resp => resp.json())
@@ -16,31 +15,9 @@ function App() {
   }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <Routes>
+      <Route path="/" element={<EventCreation/>}/>
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/admin/EventCreation.tsx
@@ -0,0 +1,10 @@
+const EventCreation = () => {
+  return (
+    <>
+      <p style={{color: 'black'}}>hi</p>
+      <button>pressme</button>
+    </>
+  );
+}
+
+export default EventCreation;

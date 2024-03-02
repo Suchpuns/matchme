47e86cc9daf34af513f62458cc57cc8906eb0609
Dylan <redman29h@gmail.com>
2024-03-02 16:02:43 +1100

    Basic implementation of preference

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,23 @@
+import json
 from app import app
+from app.graph_calc import generate_assignments
 
 @app.route("/")
 def hello_match_me():
     return {"msg": "Hello world" }
+
+@app.route("/test")
+def test():
+    roles = [("FE", 7), ("BE",3)]
+    preferences = [
+        {
+            "name": "Dylan",
+            "preference": [1,2]
+        },
+        {
+            "name": "George",
+            "preference": [2,1]
+        }
+    ] * 5
+    res = generate_assignments(roles,preferences)
+    return json.dumps(res)

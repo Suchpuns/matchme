43b1662ed6620c90ecb4b363daf00dd60a4c24b1
CHUN Nicole Wai Yan <19chunnw3@Nicole-Chun-MacBookPro-2.local>
2024-03-02 22:17:19 +1100

    finished events styling

--- frontend/src/components/EventCard.tsx
+++ frontend/src/components/EventCard.tsx
@@ -14,34 +14,55 @@ const EventCard = (props: EventCardProps) => {
   const statusItem = [
     {
       text: 'Creating event',
-      color: 'blue',
+      color: {
+        backgroundColor: 'rgba(179, 227, 242, 0.8)'
+      },
       path: 'create'
     },
     {
       text: 'Getting preferences...',
-      color: 'orange',
+      color: {
+        backgroundColor: 'rgba(244, 203, 154, 0.8)'
+      },
       path: 'preference'
     },
     {
       text: 'Completed!',
-      color: 'green',
+      color: {
+        backgroundColor: 'rgba(150, 234, 198, 0.8)'
+      },
       path: 'complete'
     }
   ]
 
   return(
-    <button onClick={() => {navigate(!isNaN(stateNum) ? `/admin/${props.name}/${statusItem[stateNum].path}` : '/')}}>
-      <p>{props.name}</p>
-      <p>{!isNaN(stateNum) ? statusItem[stateNum].text : '--'}</p>
+    <button 
+      onClick={() => {navigate(!isNaN(stateNum) ? `/admin/${props.name}/${statusItem[stateNum].path}` : '/')}}
+      className="rounded-3xl m-5 my-2 p-3 px-5 items-center hover:outline outline-2 outline-offset-2 drop-shadow-md"
+      style={statusItem[stateNum].color}>
+      
+      <div className="flex flex-col text-left">
+        <p className="text-2xl font-semibold leading-5">{props.name}</p>
+        <p>{!isNaN(stateNum) ? statusItem[stateNum].text : '--'}</p>
+      </div>
+      
 
-      <p>{props.roles}</p>
-      <p>roles</p>
+      <div className="flex flex-row m-2 mt-5 items-center justify-items-center">
+        <div className='mx-4'>
+          <p className="leading-3">{props.participants}</p>
+          <p>Participants</p>
+        </div>
+        
+        <div className='mx-4'>
+          <p className="leading-3">{props.roles}</p>
+          <p>Roles</p>
+        </div>
 
-      <p>{props.teams}</p>
-      <p>teams</p>
-
-      <p>{props.participants}</p>
-      <p>participants</p>
+        <div className='mx-4'>
+          <p className="leading-3">{props.teams}</p>
+          <p>Teams</p>
+        </div>
+      </div>
     </button>
   );
 }

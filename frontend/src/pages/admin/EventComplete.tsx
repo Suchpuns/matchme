6d90c685fc4f15b13146e97cfc9109a3c404295b
CHUN Nicole Wai Yan <19chunnw3@Nicole-Chun-MacBookPro-2.local>
2024-03-02 18:37:19 +1100

    more shit

--- frontend/src/pages/admin/EventComplete.tsx
+++ frontend/src/pages/admin/EventComplete.tsx
@@ -1,6 +1,9 @@
+import Header from "../../components/Header";
+
 const EventComplete = () => {
     return (
       <>
+        <Header eventName='hi' index='2'/>
         <p style={{color: 'black'}}>complete</p>
         <button>pressme</button>
       </>
--- frontend/src/pages/admin/Events.tsx
+++ frontend/src/pages/admin/Events.tsx
@@ -1,8 +1,54 @@
+import EventCard from "../../components/EventCard";
+
+// dummy data for now
+const events = [
+  {
+    name: 'Trainee Program',
+    status: '0',
+    roles: 12,
+    teams: 1,
+    participants: 30,
+  },
+  {
+    name: 'Atlassian',
+    status: '1',
+    roles: 12,
+    teams: 1,
+    participants: 30,
+  },
+  {
+    name: '2511 Pairings',
+    status: '1',
+    roles: 12,
+    teams: 1,
+    participants: 30,
+  },
+  {
+    name: '1531 groupings',
+    status: '2',
+    roles: 12,
+    teams: 1,
+    participants: 30,
+  }
+]
+
 const Events = () => {
+  const getEvents = events.map(x => <EventCard 
+    name={x.name}
+    status={x.status}
+    roles={x.roles}
+    teams={x.teams}
+    participants={x.participants}
+    />)
+
     return (
       <>
-        <p style={{color: 'black'}}>hi</p>
-        <button>pressme</button>
+        <h1>Welcome to MatchMe!</h1>
+        <button>
+          Create a new Event
+        </button>
+        {getEvents}
+
       </>
     );
   }

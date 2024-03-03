19da99070ee98ee5a16bbe73a0b64c7e67722530
Dylan <redman29h@gmail.com>
2024-03-03 16:33:34 +1100

    Merge

--- frontend/src/pages/admin/EventComplete.tsx
+++ frontend/src/pages/admin/EventComplete.tsx
@@ -1,16 +1,15 @@
 import { useParams } from "react-router-dom";
 import Header from "../../components/Header";
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Button from "@mui/material/Button";
 import { useEffect, useState } from "react";
 import Parser from "@json2csv/plainjs/Parser.js";
 import RoleGrouping from "../../components/RoleGrouping";
 import PersonRow from "../../components/PersonRow";
-
-const EventComplete = () => {
+const EventComplete = ({ events, setEvents }) => {
   const { eventName } = useParams();
 
   useEffect(() => {
@@ -31,117 +30,118 @@ const EventComplete = () => {
 
   const [data, setData] = useState([{"role": "Wand", "people": [{"name": "Dorian", "choice": 1}, {"name": "Dani", "choice": 3}]}, {"role": "Potion", "people": [{"name": "Monica", "choice": 1}]}, {"role": "Crystal Ball", "people": [{"name": "Nico", "choice": 1}]}])
 
-  const [groupData, setGroupData] = useState(
-    {
-      groups: [
-        {
-            groupName: "jira",
-            roles: [
-                {
-                    roleType: "Wand",
-                    roleNum: 2,
-                },
-                {
-                    roleType: "Potion",
-                    roleNum: 1,
-                }
-            ]
-        },
-        {
-            groupName: "confluence",
-            roles: [
-                {
-                    roleType: "Wand",
-                    roleNum: 1,
-                },
-                {
-                    roleType: "Crystal Ball",
-                    roleNum: 1,
-                }
-            ]
-        }
-    ]
-  })
+  const [groupData, setGroupData] = useState({
+    groups: [
+      {
+        groupName: "jira",
+        roles: [
+          {
+            roleType: "Wand",
+            roleNum: 1,
+          },
+          {
+            roleType: "Potion",
+            roleNum: 1,
+          },
+        ],
+      },
+      {
+        groupName: "confluence",
+        roles: [
+          {
+            roleType: "Wand",
+            roleNum: 1,
+          },
+          {
+            roleType: "Crystal Ball",
+            roleNum: 1,
+          },
+        ],
+      },
+    ],
+  });
 
-  const [teams, setTeams] = useState([])
+  const [teams, setTeams] = useState([]);
 
   function downloadAsCSV() {
-      console.log("clicked")
-      if (data !== null) {
-          try {
-
-              const flattened = []
-              // Transform data to csv
-              for (const role of data) {
-                for (const person of role.people) {
-                  flattened.push({"Name": person.name, "Assigned Role": role.role, "Choice": person.choice})
-                }
-              }
-              
-              const opts = { };
-              const parser = new Parser(opts);
-              const csv = parser.parse(flattened);
-
-              const blob = new Blob([csv], { type: 'text/csv;charset=utf-8' })
-              const url = URL.createObjectURL(blob)
-                
-              let pom = document.createElement('a');
-              pom.href = url;
-              pom.setAttribute('download', 'matchme.csv');
-              pom.click();
-          } catch (err) {
-              console.error(err);
+    console.log(events);
+    console.log("clicked");
+    if (data !== null) {
+      try {
+        const flattened = [];
+        // Transform data to csv
+        for (const role of data) {
+          for (const person of role.people) {
+            flattened.push({
+              Name: person.name,
+              "Assigned Role": role.role,
+              Choice: person.choice,
+            });
           }
+        }
+
+        const opts = {};
+        const parser = new Parser(opts);
+        const csv = parser.parse(flattened);
+
+        const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+        const url = URL.createObjectURL(blob);
+
+        const pom = document.createElement("a");
+        pom.href = url;
+        pom.setAttribute("download", "matchme.csv");
+        pom.click();
+      } catch (err) {
+        console.error(err);
       }
     }
+  }
 
-
-  function sortUsers(){
-
-    let flattened: any = {}
+  function sortUsers() {
+    const flattened: any = {};
     // Transform data to csv
     for (const role of data) {
       flattened[role.role] = [];
       for (const person of role.people) {
-        flattened[role.role].push(person)
+        flattened[role.role].push(person);
       }
-      flattened[role.role].sort(() => Math.random() - 0.5)
+      flattened[role.role].sort(() => Math.random() - 0.5);
     }
 
-    const groupAssignments: any = []
+    const groupAssignments: any = [];
     for (const group of groupData.groups) {
       const groupDict: any = {
-        "group_name": group.groupName,
-        "roles": []
-      }
+        group_name: group.groupName,
+        roles: [],
+      };
       for (const role of group.roles) {
         if (role.roleType in flattened) {
-          const num_to_take = Math.min(flattened[role.roleType].length, role.roleNum)
-          const people = []
-          let i = 0
+          const num_to_take = Math.min(flattened[role.roleType].length, role.roleNum);
+          const people = [];
+          let i = 0;
           while (i < num_to_take) {
-            const person = flattened[role.roleType].pop()
-            people.push(person)
+            const person = flattened[role.roleType].pop();
+            people.push(person);
             i += 1;
           }
           groupDict.roles.push({
-            "role_name": role.roleType,
-            "names": people,
-            "desired": role.roleNum
-          })
-        }        
+            role_name: role.roleType,
+            names: people,
+            desired: role.roleNum,
+          });
+        }
       }
-      groupAssignments.push(groupDict)
+      groupAssignments.push(groupDict);
     }
-    setTeams(groupAssignments)
+    setTeams(groupAssignments);
   }
 
-  useEffect(sortUsers, [])
+  useEffect(sortUsers, []);
 
   const roles = data.map(role => {
     const names = role.people.map(person => {
-      return <PersonRow person={person}/>
-    })
+      return <PersonRow person={person} />;
+    });
 
     return (
       <Accordion defaultExpanded>
@@ -153,22 +153,18 @@ const EventComplete = () => {
           <b>{role.role}</b>
         </AccordionSummary>
         <AccordionDetails>
-          <div className="flex flex-col">
-            {names}
-          </div>
+          <div className="flex flex-col">{names}</div>
         </AccordionDetails>
       </Accordion>
-    )
-  })
+    );
+  });
 
-  const groups = teams.map(group=> {
-    console.log(group)
+  const groups = teams.map(group => {
+    console.log(group);
 
     const roleList = group.roles.map(role => {
-      return (
-        <RoleGrouping role_name={role.role_name} people={role.names} desired={role.desired}/>
-      )
-    })
+      return <RoleGrouping role_name={role.role_name} people={role.names} desired={role.desired} />;
+    });
 
     return (
       <Accordion defaultExpanded>
@@ -180,17 +176,15 @@ const EventComplete = () => {
           <b>{group.group_name}</b>
         </AccordionSummary>
         <AccordionDetails>
-          <div className="flex flex-col">
-            {roleList}
-          </div>
+          <div className="flex flex-col">{roleList}</div>
         </AccordionDetails>
       </Accordion>
-    )
-  })
+    );
+  });
 
   return (
     <div>
-      <Header eventName={eventName ? eventName : 'New Event'} index='2' />
+      <Header eventName={eventName ? eventName : "New Event"} index="2" />
       <div className="h-screen">
         <div className="flex m-5 justify-between">
           <div className="mt-10 w-1/3" id="Role Assignments">
@@ -198,24 +192,28 @@ const EventComplete = () => {
           </div>
           <div className="flex">
             <div className="my-auto">
-            <Button variant="contained" className="w-64 h-64" onClick={sortUsers}>Randomly Assign teams</Button>
+              <Button variant="contained" className="w-64 h-64" onClick={sortUsers}>
+                Randomly Assign teams
+              </Button>
             </div>
           </div>
-          <div className="mt-10 w-1/3">
-            {teams === null ? <h1>Hi</h1> : groups}
-          </div>
+          <div className="mt-10 w-1/3">{teams === null ? <h1>Hi</h1> : groups}</div>
         </div>
         <div className="flex m-5 justify-between">
           <div className="w-1/3">
-              <Button className="w-full" variant="contained" onClick={(e) => downloadAsCSV()}> Download Roles</Button>
+            <Button className="w-full" variant="contained" onClick={e => downloadAsCSV()}>
+              Download Roles
+            </Button>
           </div>
           <div className="w-1/3">
-            <Button className="w-full" variant="contained" onClick={(e) => downloadAsCSV()}> Download Roles with Teams</Button>
+            <Button className="w-full" variant="contained" onClick={e => downloadAsCSV()}>
+              Download Roles with Teams
+            </Button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default EventComplete;
--- frontend/src/pages/admin/EventCreation.tsx
+++ frontend/src/pages/admin/EventCreation.tsx
@@ -1,14 +1,129 @@
-import { useParams } from "react-router-dom";
+import EditableTable from "../../components/EditableTable";
 import Header from "../../components/Header";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-const EventCreation = () => {
+interface Table {
+  [tableName: string]: string[];
+}
+
+const EventCreation = ({ getEvents, setEvents }) => {
+  const [tables, setTables] = useState<Table>({ undefined: [] });
+  const [tableName, setTableName] = useState<string>("");
   const { eventName } = useParams();
+  const navigate = useNavigate();
+  const getTableRoles = (teamName: string) => {
+    return tables[teamName];
+  };
+
+  const setTableRoles = (teamName: string, roles: string[]) => {
+    tables[teamName] = roles;
+    setTables(tables);
+  };
+
+  const updateTableName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTableName(e.target.value);
+  };
+
+  const addTable = (e: React.MouseEvent<HTMLElement>) => {
+    tables[tableName] = [];
+    setTables(tables);
+    setTableName("");
+  };
+
+  const submitTeam = async () => {
+    if (eventName === undefined) {
+      return;
+    }
+
+    // tables[table] = array of roles
+    // tables[table] = "wand", "wand", "wand"
+
+    const events = getEvents();
+    const groups = [];
+    for (const table of Object.keys(tables)) {
+      const group = { groupName: table, roles: [] };
+      if (table != "undefined") {
+        const new_roles = [];
+        for (const role of tables[table]) {
+          // const got_role = new_roles.find(role_2 => {
+          //   role_2["roleType"] === role;
+          // });
+          let got_role = undefined;
+          for (const role_2 of new_roles) {
+            if (role_2["roleType"] === role) {
+              got_role = role_2;
+            }
+          }
+
+          if (got_role != undefined) {
+            got_role["roleNum"] += 1;
+          } else {
+            new_roles.push({
+              roleType: role,
+              roleNum: 1,
+            });
+          }
+        }
+        group.roles = new_roles;
+        groups.push(group);
+      }
+    }
+    events[eventName] = groups;
+    const new_events = {};
+    new_events[eventName] = groups;
+    console.log(getEvents());
+    setEvents(events);
+    console.log(getEvents());
+    await fetch(`${import.meta.env.VITE_BE_URL}/groups`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ groups: new_events }),
+    });
+    navigate(`/admin/${eventName}/preference`);
+    return;
+  };
 
   return (
     <>
       <Header eventName={eventName ? eventName : "New Event"} index="0" />
-      <p style={{ color: "black" }}>create</p>
-      <button>pressme</button>
+      <div className="flex flex-row gap-3 flex-wrap">
+        {Object.keys(tables).map(teamName => {
+          console.log(teamName);
+          if (teamName == "undefined") {
+            return <></>;
+          }
+          return (
+            <EditableTable
+              teamName={teamName}
+              getTableRoles={getTableRoles}
+              setTableRoles={setTableRoles}
+            />
+          );
+        })}
+        <div>
+          <input
+            onChange={updateTableName}
+            value={tableName}
+            className="h-5 ml-3 mr-3 p-3 bg-card-green hover:bg-card-green w-full my-7 rounded-xl font-semibold"
+          ></input>
+          <button
+            className="p-3 ml-3 bg-card-orange hover:bg-card-green w-full my-7 rounded-xl font-semibold"
+            onClick={addTable}
+          >
+            Add team
+          </button>
+          <button
+            className="p-3 ml-3 bg-card-green hover:bg-card-orange w-full my-7 rounded-xl font-semibold"
+            onClick={submitTeam}
+          >
+            Submit teams
+          </button>
+        </div>
+      </div>
     </>
   );
 };
